2b834e179fd42a70eb506382d32068c71fbc8642
ytnobody <ytnobody@gmail.com>
2021-12-30 23:22:22 +0900

    雑にPOI追加

--- lib/Acme/TourDe/Hokkaido.pm
+++ lib/Acme/TourDe/Hokkaido.pm
@@ -2,9 +2,24 @@ package Acme::TourDe::Hokkaido;
 use 5.008001;
 use strict;
 use warnings;
+use utf8;
 
 our $VERSION = "0.01";
+our @POI = qw/
+    ハーベスター八雲
+    函館ラッキーピエロ
+    札幌ジンギスカン
+    小樽三角市場
+    室蘭カレーラーメン
+    新千歳空港で温泉
+    洞爺湖でも眺めましょう
+    長万部かなやのかにめし
+/;
 
+sub next {
+    my $k = int(rand($#POI + 1));
+    printf "%s\n", $POI[$k];
+}
 
 
 1;
@@ -19,6 +34,8 @@ Acme::TourDe::Hokkaido - A motorcycle touring plan
 =head1 SYNOPSIS
 
     use Acme::TourDe::Hokkaido;
+    my $h = Acme::TourDe::Hokkaido;
+    $h->next; ## says next point of interest
 
 =head1 DESCRIPTION
 
---
+++ run.pl
@@ -0,0 +1,9 @@
+#!perl
+use strict;
+use utf8;
+use lib 'lib';
+use Acme::TourDe::Hokkaido;
+
+binmode STDOUT, "utf8";
+
+Acme::TourDe::Hokkaido->next;
