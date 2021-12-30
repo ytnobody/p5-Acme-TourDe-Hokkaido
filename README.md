b8f185eab1e6f70b8667f1c9aea84a4090b2181b
ytnobody <ytnobody@gmail.com>
2021-12-30 21:52:06 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::TourDe::Hokkaido - It's new $module
+
+# SYNOPSIS
+
+    use Acme::TourDe::Hokkaido;
+
+# DESCRIPTION
+
+Acme::TourDe::Hokkaido is ...
+
+# LICENSE
+
+Copyright (C) ytnobody.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ytnobody <ytnobody@gmail.com>
---
+++ lib/Acme/TourDe/Hokkaido.pm
@@ -0,0 +1,39 @@
+package Acme::TourDe::Hokkaido;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::TourDe::Hokkaido - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::TourDe::Hokkaido;
+
+=head1 DESCRIPTION
+
+Acme::TourDe::Hokkaido is ...
+
+=head1 LICENSE
+
+Copyright (C) ytnobody.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=cut
+
